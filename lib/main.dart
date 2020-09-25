82636c63fd75cb37295b5cc7b517a0275647f8b0
Kingcong11 <nico.ativo@bicol-u.edu.ph>
2020-09-25 21:54:43 +0800

    Widget layout
    *added layout to widget
    *document formating shortcut learned
    *basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,12 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    var questions = [
+      'What\'s your favorite color',
+      'What\'s your favorite animal'
+    ];
+
     return MaterialApp(
       home: Scaffold( 
 
@@ -14,7 +20,27 @@ class MyApp extends StatelessWidget {
           title: Text('Notex'),
         ),
 
-        body: Text('This is the App body'),
+        body: Column(
+          children: [
+            Text('The Question.'),
+            RaisedButton(
+              child: Text('Answer1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer3'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer4'),
+              onPressed: null,
+            ),
+          ],
+        ),
 
       ),
     );
