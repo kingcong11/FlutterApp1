f62e4b8aeb5bb12f823fbdb0acec361767f69ee3
Kingcong11 <nico.ativo@bicol-u.edu.ph>
2020-10-06 11:24:51 +0800

    Reset quiz
    *added reset functionality

--- lib/result.dart
+++ lib/result.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 class Result extends StatelessWidget {
 
   final int totalScore;
+  final Function resetHandler;
 
-  Result(this.totalScore);
+  Result(this.totalScore, this.resetHandler);
 
   /* Getter */
 
@@ -29,10 +30,27 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(fontSize: 36, fontFamily: 'Painter'),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontFamily: 'Painter'),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            child: Text('Reset Quiz', style: TextStyle(fontSize: 17, color: Colors.black, letterSpacing: 1.2)),
+            onPressed: resetHandler,
+            color: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(18),
+              side: BorderSide(
+                color: Colors.black,
+                width: 4,
+                style: BorderStyle.solid,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
